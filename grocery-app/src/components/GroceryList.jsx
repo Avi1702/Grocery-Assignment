2beee35f5eb22d76dd54fd5ab52b198456929441
Avi1702 <avinashroy1702@gmail.com>
2022-06-10 11:23:23 +0530

    all parts done

--- grocery-app/src/components/GroceryList.jsx
+++ grocery-app/src/components/GroceryList.jsx
@@ -11,11 +11,12 @@ function GroceryList({data, handleDelete, handleStatus}){
   
      return (
       <>
+       
       <div key={item.id}  style={{display:"flex",justifyContent:"space-around",alignItems:"center", width:"50%",margin:"auto"}}>
-    <h2>{item.title}</h2>
+     <div> {item.status?<h2 style={{color:"green"}}>{item.title}</h2>:<h2 style={{color:"red"}}>{item.title}</h2>}</div>
     <div>-</div>
-    <h3>{item.status? "Completed":"Not Completed"}</h3>
-    <button onClick={()=>handleStatus(item.id)}>Mark As Done</button>
+    <div>{item.status?<h3 style={{color:"green"}}>Completed</h3>:<h3 style={{color:"red"}}>Not completed</h3>}</div>
+    <div>{item.status?<button onClick={()=>handleStatus(item.id)}>Done</button>:<button onClick={()=>handleStatus(item.id)}>Mark as Done</button>}</div>
     <button onClick={()=>handleDelete(item.id)}>Delete</button>
    </div>
   
